d64aa097fef1f9b4b71617c3db979cd3571f9dc4
Philip Burggasser <philburg@live.at>
2018-03-02 18:20:49 +0100

    More logging

--- ParallelProgramming/Dining Philosophers/Program.cs
+++ ParallelProgramming/Dining Philosophers/Program.cs
@@ -6,7 +6,7 @@ namespace DiningPhilosophers
 {
     public class Philosopher
     {
-        public static void Eat(object leftFork, object rightFork, int leftForkNr, int rightForkNr,
+        public void Eat(object leftFork, object rightFork, int leftForkNr, int rightForkNr,
             int philosopherNumber, int maxThinkingTime, int maxEatingTime, CancellationToken token)
         {
             Random rm = new Random();
@@ -15,26 +15,31 @@ namespace DiningPhilosophers
             {
                 // Philosopher is thinking
                 Thread.Sleep(rm.Next() % maxThinkingTime);
-                Console.WriteLine($"{philosopherNumber} philosopher finished thinking");
+                Console.WriteLine($"\t{philosopherNumber} philosopher finished thinking");
 
                 lock (leftFork)
                 {
-                    Console.WriteLine($"{philosopherNumber} philosopher took first fork {leftFork}");
+                    Console.WriteLine($"\t\t{philosopherNumber} philosopher took first fork {leftForkNr}");
                     lock (rightFork)
                     {
-                        Console.WriteLine($"{philosopherNumber} philosopher took second fork {rightFork}");
+                        Console.WriteLine($"\t\t\t{philosopherNumber} philosopher took second fork {rightForkNr}");
                         Thread.Sleep(rm.Next() % maxEatingTime);
-                        Console.WriteLine($"{philosopherNumber} philosopher finished eating");
+                        Console.WriteLine($"\t\t\t{philosopherNumber} philosopher finished eating");
                     }
+                    Console.WriteLine($"\t\t{philosopherNumber} philosopher put fork {rightForkNr} back");
                 }
+                Console.WriteLine($"\t{philosopherNumber} philosopher put fork {leftForkNr} back");
             }
-            
+
             Console.WriteLine($"{philosopherNumber} philosopher left task");
         }
     }
-    
+
+
     class Program
     {
+        public static object[] forks;
+
         static void Main(string[] args)
         {
             int n, thinkingTime, eatingTime;
@@ -52,26 +57,37 @@ namespace DiningPhilosophers
             consoleInput = Console.ReadLine();
             eatingTime = int.Parse(consoleInput);
 
+            Console.WriteLine();
+
             CancellationTokenSource source = new CancellationTokenSource();
 
-            object[] forks = new object[n];
+            forks = new object[n];
             for (int i = 0; i < n; i++)
             {
                 forks[i] = new object();
             }
 
             Task[] pTasks = new Task[n];
-            pTasks[0] = new Task(() => Philosopher.Eat(forks[n - 1], forks[0],
-                    n- 1, 0, 1, thinkingTime, eatingTime, source.Token));
+            pTasks[0] = new Task(() =>
+            {
+                Philosopher p = new Philosopher();
+                p.Eat(forks[n - 1], forks[0],
+                    n - 1, 0, 0, thinkingTime, eatingTime, source.Token);
+            });
             pTasks[0].Start();
 
             for (int i = 1; i < n; i++)
             {
-                pTasks[i] = new Task(() => Philosopher.Eat(forks[i - 1], forks[i % n],
-                    i - 1, i, i + 1, thinkingTime, eatingTime, source.Token));
+                int ix = i;
+                pTasks[i] = new Task(() =>
+                {
+                    Philosopher p = new Philosopher();
+                    p.Eat(forks[ix - 1], forks[ix],
+                        ix - 1, ix, ix, thinkingTime, eatingTime, source.Token);
+                });
                 pTasks[i].Start();
             }
-            
+
             // Let them eat
             Thread.Sleep(1000);
 
