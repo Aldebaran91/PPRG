0ab809ba9118d36a080915f07ce49e54854ef7d1
Philip Burggasser <philburg@live.at>
2018-03-17 09:40:31 +0100

    Added quick- and mergesort

--- ParallelProgramming/Task_Parallelism/Program.cs
+++ ParallelProgramming/Task_Parallelism/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MergeSort;
+using QuickSort;
+using System;
+using System.Diagnostics;
 
 namespace Task_Parallelism
 {
@@ -10,6 +9,46 @@ namespace Task_Parallelism
     {
         static void Main(string[] args)
         {
+            const int lenght = 15;
+            const int laps = 1_000_000;
+
+            byte[] testArray = new byte[lenght];
+            Random rdm = new Random();
+            rdm.NextBytes(testArray);
+
+            var firstQuickSort = (byte[])testArray.Clone();
+            var quickSort = new QuickSort<byte>();
+            quickSort.Sort(firstQuickSort);
+
+            var firstMergeSort = (byte[])testArray.Clone();
+            var mergeSort = new MergeSort<byte>();
+            mergeSort.Sort(firstMergeSort);
+
+            Stopwatch quickSortTime = new Stopwatch();
+            Stopwatch mergeSortTime = new Stopwatch();
+
+            for (int i = 0; i < laps; i++)
+            {
+                byte[] temp = new byte[lenght];
+                rdm.NextBytes(temp);
+                quickSortTime.Start();
+                quickSort.Sort(temp);
+                quickSortTime.Stop();
+            }
+
+            for (int i = 0; i < laps; i++)
+            {
+                byte[] temp = new byte[lenght];
+                rdm.NextBytes(temp);
+                mergeSortTime.Start();
+                mergeSort.Sort(temp);
+                mergeSortTime.Stop();
+            }
+
+            Console.WriteLine($"QuickSort time= {quickSortTime.ElapsedMilliseconds:N0} ms");
+            Console.WriteLine($"MergeSort time= {mergeSortTime.ElapsedMilliseconds:N0} ms");
+
+            Console.ReadKey(false);
         }
     }
 }
