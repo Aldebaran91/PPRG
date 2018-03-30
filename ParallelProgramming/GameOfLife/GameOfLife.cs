67ec12407b4cbbdd42de36464412417ad9302fed
Paul Vonbank <vonbank@gmail.com>
2018-03-30 22:12:29 +0200

    GoL init

---
+++ ParallelProgramming/GameOfLife/GameOfLife.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class GameOfLife
+    {
+        public bool[,] CurrentState { get; set; }
+
+        public GameOfLife(int SizeX, int SizeY, int Iterations)
+        {
+
+        }
+
+        public GameOfLife(bool[,] InitialField, int Iterations)
+        {
+
+        }
+    }
+}
